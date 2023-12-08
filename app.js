950c5c35d582c519c6618e06e7b2803f3dd69f82
Tabarak2001 <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2023-12-08 19:24:07 +0300

    Good-challenge

--- app.js
+++ app.js
@@ -1,60 +1,94 @@
-const experee = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const app = experee();
+const app = express();
 const port = 3000;
 
-// Use middleware to parse JSON in the request body
-app.use(bodyParser.json());
-app.get('/',(req, res)=>
- res.send("This is my first Backend test")
-)
+app.use(express.json());
 
 // Secret key for signing and verifying JWT
-const SecretKey = 'your-secret-key';
+const secretKey = '1973TAR';
 
-const mockUser = {
-    username: 'TabarakAli',
-    password: '2001@star@GalaxyGirl'
-};
-//Login endpoint
-app.post('login', (req, res) => {
-    const { username, password } = req.body;
+// Fake user list
+const fakeUsers = [
+    {
+        username: 'TabarakAli',
+        password: '2001@star@GalaxyGirl',
+        email: 'aaltwt@gmail.com'
+    },
+    {
+        name:'Nabaa',
+        password:'333',
+        email:'nnnnnn@gamil.com',
+    },
+    {
+        name:'Mortadaa',
+        password:'4444',
+        email:'mmmmmm@gamil.com',
+    }
+];
 
-    if (username === mockUser.username && password === mockUser.password) {
-        //Generate JWT token
-        const token = jwt.sign({ username: mockUser.username }, SecretKey, { expiresIn: '1h' });
+// Login endpoint
+app.post('/login', (req, res) => {
+    const { username, password, email } = req.body;
+
+    // Check if provided credentials match any fake user
+    const matchedUser = fakeUsers.find(
+        user =>
+            user.username === username &&
+            user.password === password &&
+            user.email === email
+    );
+
+    if (!matchedUser) {
+        // Generate JWT token
+        const token = jwt.sign({ username: username }, 'shhhhh', {
+            algorithm: 'HS256' ,
+            expiresIn: '1h'
+        });
         res.json({ token });
     } else {
         res.status(401).json({ error: 'Invalid credentials' });
     }
 });
+
 // Example protected route using JWT
 app.get('/protected', authenticateToken, (req, res) => {
     res.json({ message: 'You have access to this protected route!' });
-  });
-  
-  // Middleware to authenticate JWT token
-  function authenticateToken(req, res, next) {
-    const token = req.headers['authorization'];
-  
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized - Token not provided' });
-    }
-  
-    jwt.verify(token, SecretKey, (err, user) => {
-      if (err) {
-        return res.status(403).json({ error: 'Forbidden - Invalid token' });
-      }
-  
-      req.user = user;
-      next();
-    });
+});
 
-  }
-  
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${3000}`);
-  });
+// Middleware to authenticate JWT token
+
+function authenticateToken(req, res, next) {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized - Token not provided' });
+    }
+
+
+
+    jwt.verify(token,'shhhhh',{algorithms:['HS256']}, (err, decoded) => {
+        if (err) {
+
+           
+
+            return res.status(403).json({ error: 'Forbidden - Invalid token' +
+            
+            secretKey+decoded+token});
+        }
+
+        req.user = decoded;
+        next();
+    });
+}
+
+
+
+
+
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
