e6d5ccb03e31ad72783cafa2c82dddfbe41eac1d
Tabarak2001 <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2023-12-10 14:34:21 +0300

    Good-Challenge

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-
+const cors = require('cors')
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
+const crypto =require('crypto');
 
+// Generate a random secret key
 // Secret key for signing and verifying JWT
-const secretKey = '1973TAR';
+const secretKey =  crypto.randomBytes(32).toString('hex');
 
 // Fake user list
 const fakeUsers = [
     {
-        username: 'TabarakAli',
-        password: '2001@star@GalaxyGirl',
-        email: 'aaltwt@gmail.com'
+        username: 'Tabarak',
+        password: 'star',
+        email: 'aaltwt49@gmail.com'
     },
     {
-        name:'Nabaa',
-        password:'333',
-        email:'nnnnnn@gamil.com',
+        username:'Nabaa',
+        password:'333nnn',
+        email:'nnnnnn@gmail.com',
     },
     {
-        name:'Mortadaa',
-        password:'4444',
-        email:'mmmmmm@gamil.com',
+        username:'Mortadha',
+        password:'4:44',
+        email:'mmmmmm@gmail.com',
+    },
+    {
+        username:'toto',
+        password:'toto',
+        email:'toto@gmail.com',
     }
 ];
 
@@ -40,11 +47,10 @@ app.post('/login', (req, res) => {
             user.email === email
     );
 
-    if (!matchedUser) {
+    if (matchedUser) {
         // Generate JWT token
-        const token = jwt.sign({ username: username }, 'shhhhh', {
-            algorithm: 'HS256' ,
-            expiresIn: '1h'
+        const token = jwt.sign({username: username ,password: password, email: email }, secretKey, {
+            expiresIn: '24h'
         });
         res.json({ token });
     } else {
@@ -57,38 +63,37 @@ app.get('/protected', authenticateToken, (req, res) => {
     res.json({ message: 'You have access to this protected route!' });
 });
 
-// Middleware to authenticate JWT token
+// Middleware to authenticate JWT token 
 
+//Decode and login the received token
 function authenticateToken(req, res, next) {
     const token = req.headers['authorization'];
+    console.log('Received Token:' , token)
 
     if (!token) {
         return res.status(401).json({ error: 'Unauthorized - Token not provided' });
     }
 
+    const trimmedToken = token.trim();
+    const decodedToken = jwt.decode(trimmedToken, {complete: true});
+    console.log('Decoded Token:', decodedToken);
 
-
-    jwt.verify(token,'shhhhh',{algorithms:['HS256']}, (err, decoded) => {
-        if (err) {
-
-           
-
-            return res.status(403).json({ error: 'Forbidden - Invalid token' +
-            
-            secretKey+decoded+token});
-        }
-
-        req.user = decoded;
-        next();
-    });
+    if(!decodedToken){
+        console.error('JWT Verification Error');
+        return res.status(403).json({ error: 'Forbidden - Invalid token' });
+    }
+else{
+    req.user = decodedToken.payload.user;
+    return res.status(200).json({"name":decodedToken.payload.username,"password":decodedToken.payload.password,"email":decodedToken.payload.email});
+  
+   // next();
 }
+         
+   
+    }
 
 
-
-
-
-
-// Start the server
+    // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
